0db8db4877f484f875ce6a022c242d8c3e72c2fe
IsmaLB <61549609+Isma-LB@users.noreply.github.com>
2025-01-09 12:49:53 -0500

    Add multiple planets scenes and multi scene helper

---
+++ NestedWorlds_unity/Assets/Scripts/Editor/EditorWindows/MultiSceneHelper.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace IsmaLB.Editors
+{
+    public class MultiSceneHelper : EditorWindow
+    {
+        private Object baseScene = null;
+        private Object planetScene = null;
+        private bool autoLoad = true;
+
+        [MenuItem("NestedWorlds/MultiSceneHelper")]
+        private static void ShowWindow()
+        {
+            var window = GetWindow<MultiSceneHelper>();
+            window.titleContent = new GUIContent("MultiSceneHelper");
+            window.Show();
+        }
+        private void OnGUI()
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Base scene", EditorStyles.label);
+            baseScene = EditorGUILayout.ObjectField(baseScene, typeof(SceneAsset), false);
+            GUILayout.EndHorizontal();
+
+            EditorGUI.BeginChangeCheck();
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("PlanetScene", EditorStyles.label);
+            planetScene = EditorGUILayout.ObjectField(planetScene, typeof(SceneAsset), false);
+            GUILayout.EndHorizontal();
+
+            if (EditorGUI.EndChangeCheck() && autoLoad && baseScene != null && planetScene != null)
+            {
+                LoadMultiSceneSetup();
+            }
+            autoLoad = EditorGUILayout.Toggle("Auto Load on planet selection", autoLoad);
+            if (GUILayout.Button("Load Scenes"))
+            {
+                LoadMultiSceneSetup();
+            }
+        }
+        void LoadMultiSceneSetup()
+        {
+            if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+            {
+                EditorSceneManager.OpenScene(AssetDatabase.GetAssetPath(baseScene));
+                EditorSceneManager.OpenScene(AssetDatabase.GetAssetPath(planetScene), OpenSceneMode.Additive);
+            }
+        }
+    }
+}
