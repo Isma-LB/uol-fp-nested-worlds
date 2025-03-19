72d903da5917cdde32a7a4d733d9a711020bb63c
IsmaLB <61549609+Isma-LB@users.noreply.github.com>
2025-03-19 17:51:42 -0500

    Feat(HUD): add pause button

--- NestedWorlds_unity/Assets/Scripts/UI/GameUIManager.cs
+++ NestedWorlds_unity/Assets/Scripts/UI/GameUIManager.cs
@@ -7,18 +7,37 @@ namespace IsmaLB.UI
 {
     public class GameUIManager : MonoBehaviour
     {
-        [SerializeField] InputReader inputReader;
+        [Header("Manages:")]
         [SerializeField] PauseScreenController pauseScreenController;
+        [SerializeField] UIPanel explorationUI;
+        [Header("Listens on:")]
+        [SerializeField] InputReader inputReader;
+        [SerializeField] TransitionEventSO loadPuzzleEvent;
+        [SerializeField] TransitionEventSO unloadPuzzleEvent;
         void OnEnable()
         {
             inputReader.pauseMenuEvent += OpenPauseScreen;
+            loadPuzzleEvent.OnTransitionStarted += EnablePuzzleUI;
+            unloadPuzzleEvent.OnTransitionEnded += EnableExplorationUI;
+
         }
         void OnDisable()
         {
             inputReader.pauseMenuEvent -= OpenPauseScreen;
+            loadPuzzleEvent.OnTransitionStarted -= EnablePuzzleUI;
+            unloadPuzzleEvent.OnTransitionEnded -= EnableExplorationUI;
         }
 
-        private void OpenPauseScreen()
+        private void EnablePuzzleUI()
+        {
+            explorationUI.Close();
+        }
+        private void EnableExplorationUI()
+        {
+            explorationUI.Open();
+        }
+
+        public void OpenPauseScreen()
         {
             pauseScreenController.Open(OnPauseScreenClose);
             Time.timeScale = 0;
